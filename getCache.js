d411acbaa771d224a6c59160d750b9fbd36f9e1d
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-04-09 22:50:04 +0300

    feature: fetch any cached Tribus

--- getCache.js
+++ getCache.js
@@ -1,15 +1,25 @@
 const arweave = Arweave.init({})
 
 async function getCacheData() {
-	const archiver_addr = "the public address of the wallet you are using to cache the data"
-//   get the last cache data JSON object
+	const archiver_addr = "rB86LBc5uSm68ZGh5v2sdiM-8r4uFOZ35VZwBvnesqo"
 	const last_tx = await arweave.wallets.getLastTransactionID(archiver_addr)
 
 	const last_cache_data = await arweave.transactions.getData(last_tx, {decode: true, string: true});
+
 	const cache_object = JSON.parse(last_cache_data);
 	const cache = new Map( Object.entries(cache_object) );
 
-	return cache
+
+	const hash = window.location.hash.substring(1);
+	if (hash) {
+		if ( cache.has(hash) ) {
+			const data =  cache.get(hash)
+			return await arweave.transactions.getData(data, {decode: true, string: true})
+		}
+	} else {
+		const data =  cache.get("null");
+		return await arweave.transactions.getData(data, {decode: true, string: true})
+	}
 
 	
 };
